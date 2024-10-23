91b6e91bd2ea80006029a3fda084925e72759bcd
Paul Arden <parden@made-f0Dr9s9.clusters.42paris.fr>
2024-10-23 15:11:19 +0200

    rewriting

--- old_client.c
+++ old_client.c
@@ -1,4 +1,9 @@
-#include "minitalk.h"
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <signal.h>
+#include <stdio.h>
+
 
 static void	handler(int signal)
 {
@@ -10,11 +15,11 @@ void	establish_connection(pid_t server_pid)
 	pause();
 }
 
-void	send_len(pid_t server_pid, int len)
+void	send_len(pid_t server_pid, size_t len)
 {
 	int	i;
 
-	i = 8 * sizeof(int);
+	i = 8 * sizeof(size_t);
 	while (i)
 	{
 		i--;
@@ -51,15 +56,16 @@ void	send_string(pid_t server_pid, char *msg)
 int	main(int argc, char **argv)
 {
 	struct sigaction	act;
-	pid_t				server_pid;
+	pid_t				server_pid = atoi(argv[1]);
+	size_t				len = strlen(argv[2]);
 
-	if (argc != 3)
-		return (write(2, "Usage: ./client pid msg\n", 24));
-	server_pid = ft_strtoll(argv[1], 0, 0);
-	if (server_pid == -1)
-		return (write(2, "I'd rather not...\n", 18));
-
-	act.sa_flags = SA_SIGINFO;
+	memset(&act, 0, sizeof(act));
+	act.sa_handler = handler;
+	sigaction(SIGUSR1, &act, NULL);
+	sigaction(SIGUSR2, &act, NULL);
+	establish_connection(server_pid);
+	send_len(server_pid, len);
+	send_string(server_pid, argv[2]);
 
 	write(1, "Great Success\n", 15);
 }
